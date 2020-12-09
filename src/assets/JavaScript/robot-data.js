9ce9c2b92f1d2c866a95fe6ce91a0c84378c463a
chenguihui1 <2404296720@qq.com>
2020-12-09 23:47:46 +0800

    main

--- src/assets/JavaScript/robot-data.js
+++ src/assets/JavaScript/robot-data.js
@@ -1,7 +1,10 @@
 // 时间对象获取
 const data = [
-  { id: 1, content: '商品质量' },
-  { id: 2, content: '今晚没吃饭' },
-  { id: 3, content: '今晚加班' }
+  { id: 1, msg: '商品质量' },
+  { id: 2, msg: '今晚没吃饭' },
+  { id: 3, msg: '今晚加班' }
 ]
-export default data
+
+export const information = () => {
+  return data
+}
